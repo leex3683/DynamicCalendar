31e73a503e684791c9f76d846a8d62a718425634
unknown <leex3683@umn.edu>
2023-07-15 11:50:36 -0500

    added current day at top. Fixed row colors

--- script.js
+++ script.js
@@ -1,6 +1,49 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+
+
+setInterval(function () {
+
+
+  let today = dayjs();
+  let timePart = 0;
+  let ampmPart = 0;
+  let welcome = $('#currentDay').text("Today is " + today.format('dddd, MMMM, D'));
+
+
+  for (let i = 9; i < 18; i++) {
+    let time = $('#hour-' + i).children('.hour').text();
+
+
+    if (time.length == 3) {
+      timePart = Number(time.slice(0, 1));
+      ampmPart = time.slice(1, 3);
+    } else {
+      timePart = Number(time.slice(0, 2));
+      ampmPart = time.slice(2, 4);
+    }
+
+    
+    if (ampmPart == 'PM' && timePart !== 12) { timePart = timePart + 12 };
+
+    console.log(timePart + ampmPart);
+
+
+
+    if (timePart > today.format('HH')) {
+      $('#hour-' + i).children('textarea').addClass("future");
+    } else if (timePart == today.format('HH')) {
+      $('#hour-' + i).children('textarea').addClass("present");
+    } else if (timePart < today.format('HH')) {
+      $('#hour-' + i).children('textarea').addClass("past");
+    }
+  }
+
+
+}, 1000);
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
