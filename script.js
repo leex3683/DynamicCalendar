9a6cfe9afd89fe05e26eaf60e2bdbef8a19c90a8
unknown <leex3683@umn.edu>
2023-07-15 20:23:06 -0500

    hooked up save button to local storage. Made the browser pull from local storage when loaded. Added fun effects to save button. Accounted for times outside of 9-5

--- script.js
+++ script.js
@@ -1,59 +1,28 @@
-
-
 $(function () {
   //dynamically change colors of calendar and current date
   setInterval(function () {
-  today = dayjs();
-
+    today = dayjs();
     $('#currentDay').text("Today is " + today.format('dddd, MMMM, D'))
-    let nowBlock =$('#hour-' + today.format('H'));
+    let nowBlock = $('#hour-' + today.format('H'));
+    if (today.format('H') < 9) {
+      $(".row").addClass('future');
+    }
+    else if (today.format('H') > 17) {
+      $(".row").addClass('past');
+    }
     
+    else {
     nowBlock.addClass("present");
     nowBlock.nextAll().addClass("future");
     nowBlock.prevAll().addClass("past");
+    }
+  }, 100);
 
-    // let today = dayjs();
-    // let timePart, ampmPart = 0;
-    // //Dynamically set current date at top of screen
-    // $('#currentDay').text("Today is " + today.format('dddd, MMMM, D'));
-
-    // //loop through each row (with tag hour-i) and compare the text with the current date. Add class past, future, or present accordingly
-    // for (let i = 9; i < 18; i++) {
-    //   let time = $('#hour-' + i).children('.hour').text();
-    //   if (time.length == 3) {
-    //     timePart = Number(time.slice(0, 1));
-    //     ampmPart = time.slice(1, 3);
-    //   } else {
-    //     timePart = Number(time.slice(0, 2));
-    //     ampmPart = time.slice(2, 4);
-    //   }
-    //   //convert AM PM formatted time to military by adding 12 to timePart when 1pm or later
-    //   if (ampmPart == 'PM' && timePart !== 12) { timePart = timePart + 12 };
-    //   //compart military to military and add class accordingly
-    //   if (timePart > today.format('HH')) {
-    //     $('#hour-' + i).children('textarea').addClass("future");
-    //   } else if (timePart == today.format('HH')) {
-    //     $('#hour-' + i).children('textarea').addClass("present");
-    //   } else if (timePart < today.format('HH')) {
-    //     $('#hour-' + i).children('textarea').addClass("past");
-    //   }
-    // }
-
-
-  }, 1000);
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
+  //on button click, save the text value to local storage with key equal to parent id
   $('button').click(function () {
-
     $(this).siblings('textarea').text($(this).siblings('textarea').val());
-
-    let successMessageEl = $('<p>').text("Saved to local Storage").css("font-size", "20px");
+    localStorage.setItem($(this).parents().attr('id'), $(this).siblings('textarea').text());
+    let successMessageEl = $('<p>').text("Saved to local Storage").css("font-size", "15px");
     $(this).children('.fas').hide(0);
     $(this).append(successMessageEl);
     successMessageEl.fadeOut(2000);
@@ -61,12 +30,8 @@ $(function () {
       $(this).siblings('.fas').show(1000)
     })
   })
-
-
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  //On page load, get all locally stored text and put it in the div with key matching id
+  for (let j = 9; j < 18; j++) {
+    $("#hour-" + j).children('textarea').text(localStorage.getItem("hour-" + j))
+  }
 });
